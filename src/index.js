7fefa7298170a92137f15dcbe05739343da13c9d
Tokiimugi <tokiimugi@gmail.com>
2022-03-29 16:30:33 -0400

    testing database

--- src/index.js
+++ src/index.js
@@ -5,139 +5,33 @@ import { NavBar } from './components/NavBar';
 import { Blog } from './components/Blog';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set } from "firebase/database";
+
+// TODO: Replace with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD2GUIHUfINxar06G7aJeDes_T4sQLh7dk",
+    authDomain: "tokiimugi-website.firebaseapp.com",
+    databaseURL: "https://tokiimugi-website-default-rtdb.firebaseio.com",
+    projectId: "tokiimugi-website",
+    storageBucket: "tokiimugi-website.appspot.com",
+    messagingSenderId: "410123432207",
+    appId: "1:410123432207:web:885a58ede6e326ba578e7a",
+    measurementId: "G-C7FL9QS1FW"
+  };
+
+const app = initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+const database = getDatabase(app);
 
 
-function Square(props) {
-    return (
-        <button
-            className="square"
-            onClick={props.onClick}
-        >
-            {props.value}
-        </button>
-    );
-}
-
-class Board extends React.Component {
-
-
-
-    renderSquare(i) {
-        return (
-            <Square
-                value={this.props.squares[i]}
-                onClick={() => this.props.onClick(i)}
-            />
-        );
-    }
-
-    render() {
-
-        return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
-}
-
-class Game extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            history: [{
-                squares: Array(9).fill(null),
-            }],
-            xIsNext: true,
-        };
-    }
-
-    handleClick(i) {
-        const history = this.state.history;
-        const current = history[history.length -1]; 
-        const squares = current.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            history: history.concat([{
-                squares: squares,
-            }]),
-            xIsNext: !this.state.xIsNext,
+const Send = ()=>{
+    function handleButton(){
+        set(ref(database, 'test'),{
+            test: test
         })
     }
-    render() {
-        const history = this.state.history;
-        const current = history[history.length -1]; 
-        const winner = calculateWinner(current.squares);
-        
-        const moves = history.map((step,move)=>{
-            const desc = move ? `Go to move #${move}`:
-            'Go to game start';
-            return(
-                <li>
-                    <button onClick={()=>this.jumpTo(move)}>{desc}</button>
-                </li>
-            )
-        })
-        
-        
-        let status;
-        if (winner) {
-            status = `Winner ${winner}`
-        } else {
-            status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
-        }
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <Board 
-                        squares={current.squares}
-                        onClick={(i) => this.handleClick(i)}
-                    />
-                </div>
-                <div className="game-info">
-                    <div>{status}</div>
-                    <ol>{moves}</ol>
-                </div>
-            </div>
-        );
-    }
-}
-
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ]
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    return null;
 }
 
 // ========================================
@@ -145,7 +39,7 @@ function calculateWinner(squares) {
 const items = {
     Home: <Blog/>,
     Portfolio: <p>test</p>,
-    About: <Game/>,
+    About: <Send/>,
 }
 
 const Root = (props) => {
